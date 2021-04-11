ba4a32a0128791ebe09d4dd42865b6f90845e2a3
Tomos <Willistomos@gmail.com>
2021-04-11 15:38:27 +0100

    Name chnages

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,20 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model
+class Category extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
     public function products()
     {
-        return $this->belongsToMany(products::class)
-            ->withPivot('quantity');
+        return $this->belongsToMany(Product::class);
     }
 }
